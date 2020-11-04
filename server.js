e8a531816f9078278e580f014dfa9c1aaee3981c
Cengizhan KOSE <kose651@gmail.com>
2020-11-04 12:10:52 +0300

    pusher and realtime add

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
+//import
 import express from "express";
 import mongoose from "mongoose";
 import Messages from "./dbMessages.js";
+import Pusher from "pusher";
+import cors from "cors";
 
+//app config
 const app = express();
 const port = process.env.PORT || 9000;
 
-app.use(express.json());
+const pusher = new Pusher({
+  appId: "1101830",
+  key: "70949a70c894df6fa302",
+  secret: "03f7ebd83102d9fc0a66",
+  cluster: "eu",
+  useTLS: true,
+});
 
+//middleware
+app.use(express.json());
+app.use(cors());
+
+//DB config
 const connection_url =
   "mongodb+srv://admin:cpcPH4aTBss0B2vb@cluster0.xjyvm.mongodb.net/whatsappdb?retryWrites=true&w=majority";
 mongoose.connect(connection_url, {
@@ -15,6 +30,30 @@ mongoose.connect(connection_url, {
   useUnifiedTopology: true,
 });
 
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("DB connected");
+
+  const msgCollection = db.collection("messagecontents");
+  const changeStream = msgCollection.watch();
+
+  changeStream.on("change", (change) => {
+    console.log("a change occured", change);
+    if (change.operationType === "insert") {
+      const messageDetails = change.fullDocument;
+      pusher.trigger("messages", "inserted", {
+        name: messageDetails.name,
+        message: messageDetails.message,
+        timestamp: messageDetails.timestamp,
+        received: messageDetails.received,
+      });
+    } else {
+      console.log("Error triggering in Pusher");
+    }
+  });
+});
+//api routes
 app.get("/", (req, res) => res.status(200).send("CENGO"));
 
 app.get("/messages/sync", (req, res) => {
